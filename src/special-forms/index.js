486a9e56e643639aced1c6ab9052996cb5753697
Eric Lifka <eric.lifka@gmail.com>
2021-12-14 16:07:53 -0500

    let version of a for loop

--- src/special-forms/index.js
+++ src/special-forms/index.js
@@ -25,6 +25,7 @@ export default [
     ifForm,
     importForm,
     letForm,
+    loopForm,
     moduleForm,
     quoteForm,
     ...runnables,
--- src/special-forms/loop.js
+++ src/special-forms/loop.js
@@ -11,15 +11,95 @@ goals for this special form:
   statement2
   ...)
 
-(loop :let i :in (some-collection)
-  statement1
-  statement2
-  ...)
+(loop :let (i :in (some-collection)) ...)
+(loop :let (i :of (some-collection)) ...)
+(loop :let ((key val) :in (some-collection)) ...)
 
 */
 
-import { specialFormType, tokenType } from '../types/types'
+import { assert } from '../assert'
+import { arrayOfArguments } from '../destructuring'
+import { runCode } from '../eval'
+import { createScope, setOnScope } from '../scope'
+import { hashmapGet } from '../types/hashmap'
+import { addToList, cons, listFirst, listGetAtIndex, listRest } from '../types/list'
+import { functionType, listType, numberType, specialFormType, tokenType, TYPE } from '../types/types'
+import { vectorFromList } from '../types/vector'
 
-function loopSpecialForm(code) {}
+function runForLoop(special, body, scope) {}
+function runWhileLoop(special, body, scope) {}
+
+function runInLoop(special, body, scope) {
+    let [tokens, _in, collectionStmnt] = arrayOfArguments(special, 3)
+    let collection = runCode(collectionStmnt, scope)
+    let key, val
+    if (tokens.type === TYPE.list) {
+        key = listGetAtIndex(tokens, 0)
+        val = listGetAtIndex(tokens, 1)
+    } else if (_in.value === ':in') {
+        val = tokens
+    } else if (_in.value === ':of') {
+        key = tokens
+    } else {
+        assert(false, `:let loop invalid format of special list`)
+    }
+
+    let collectionArray
+    if (collection.type === TYPE.vector) {
+        collectionArray = collection.value
+    } else if (collection.type === TYPE.list) {
+        collectionArray = vectorFromList(collection).value
+    } else if (collection.type === TYPE.hashmap) {
+        collectionArray = collection.keys
+    }
+
+    let result = listType()
+    let loopScope = createScope(
+        [
+            [
+                tokenType('collect'),
+                functionType(`(collect entity)`, (args) => {
+                    addToList(result, listGetAtIndex(args, 0))
+                }),
+            ],
+        ],
+        scope
+    )
+
+    for (let i = 0; i < collectionArray.length; i++) {
+        let k = numberType(i)
+        let v = collectionArray[i]
+        if (collection.type === TYPE.hashmap) {
+            k = v
+            v = hashmapGet(collection, k)
+        }
+        key && setOnScope(loopScope, key, k)
+        val && setOnScope(loopScope, val, v)
+
+        runCode(body, loopScope)
+    }
+
+    return result
+}
+
+function loopSpecialForm(code, scope) {
+    let key = listFirst(code)
+    let loopSpecial = listFirst(listRest(code))
+    let loopBody = cons(tokenType('do'), listRest(listRest(code)))
+
+    switch (key.value) {
+        case ':for':
+            return runForLoop(loopSpecial, loopBody, scope)
+
+        case ':while':
+            return runWhileLoop(loopSpecial, loopBody, scope)
+
+        case ':let':
+            return runInLoop(loopSpecial, loopBody, scope)
+
+        default:
+            assert(false, `Invalid looping type: ${key.type}|${key.value}`)
+    }
+}
 
 export default [tokenType('loop'), specialFormType(`<loop special form>`, loopSpecialForm)]
